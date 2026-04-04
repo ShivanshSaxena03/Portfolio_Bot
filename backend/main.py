e6bc0ad9e8ee9acafbc1596d051ee15cc9f594b4
ShivanshSaxena03 <shivanshsaxena03102006@gmail.com>
2026-04-04 19:19:24 +0530

    all cors allowed

--- backend/main.py
+++ backend/main.py
@@ -31,11 +31,7 @@ app.add_middleware(SlowAPIMiddleware)
 # ===== CORS =====
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "http://localhost:5173",
-    "https://shivanshsaxena03.vercel.app/",
-    "https://shivanshsaxena03.vercel.app"
-],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
